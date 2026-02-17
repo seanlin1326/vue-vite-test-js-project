d580f51bf3a8e9314a69afb9676008f7beb4c9bd
軒宇的筆電\seanl <seanlin1326@gmail.com>
2026-02-17 13:21:32 +0800

    Feat:加入自動排版插件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  
   <v-app>
     <v-main>
-    <nav>
-      <!-- router-link 標籤讓使用者點擊後切換到不同的頁面 -->
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <!-- 不同的頁面切換顯示的區域 -->
-    </nav>
-    <router-view />  
+      <nav>
+        <!-- router-link 標籤讓使用者點擊後切換到不同的頁面 -->
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+        <!-- 不同的頁面切換顯示的區域 -->
+      </nav>
+      <router-view />
     </v-main>
   </v-app>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,36 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    {{ passwordIsValid }} {{passwordInputValue}}
-      <v-container>
-    <v-text-field
+    {{ passwordIsValid }} {{ passwordInputValue }}
+    <v-container>
+      <v-text-field
         v-model="passwordInputValue"
         label="輸入密碼"
         type="password"
       ></v-text-field>
 
-    
-     <v-btn color="primary" :disabled="!passwordIsValid" @click="alertTest">確認</v-btn>
-     
-   </v-container>
+      <v-btn color="primary" :disabled="!passwordIsValid" @click="alertTest"
+        >確認</v-btn
+      >
+    </v-container>
   </div>
 </template>
 
-<script setup  lang="js">
-import Swal from 'sweetalert2'
-import { ref , watch } from 'vue' 
-  const passwordIsValid = false;
-  const passwordInputValue = ref('');
-  const alertTest = ()=>{
-    Swal.fire({
-    title: 'Hello 👋',
-    text: '這是 SweetAlert2 彈窗',
-    icon: 'success',
-    confirmButtonText: '確定',
-    heightAuto:false
-  })
-  }
-  watch(passwordInputValue,(newValue,oldValue)=>{
-     console.log('new:', newValue, 'old:', oldValue)
-  })
-
+<script setup lang="js">
+import Swal from "sweetalert2";
+import { ref, watch } from "vue";
+const passwordIsValid = false;
+const passwordInputValue = ref("");
+const alertTest = () => {
+  Swal.fire({
+    title: "Hello 👋",
+    text: "這是 SweetAlert2 彈窗",
+    icon: "success",
+    confirmButtonText: "確定",
+    heightAuto: false,
+  });
+};
+watch(passwordInputValue, (newValue, oldValue) => {
+  console.log("new:", newValue, "old:", oldValue);
+});
 </script>
